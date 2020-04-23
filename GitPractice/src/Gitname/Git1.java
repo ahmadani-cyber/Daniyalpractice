427c3cf375640824b1fcd3e841d4df7691df7df4
Daniyal <butt.daniyalahmad1992@gmail.com>
2020-04-23 12:07:36 -0400

    1st commits

--- GitPractice/src/Gitname/Git1.java
+++ GitPractice/src/Gitname/Git1.java
@@ -4,9 +4,8 @@ public class Git1 {
 
 	public static void main(String[] args) {
 		
-		
-		System.out.println("Daniyal");
-		
+		System.out.println("Daniyal Ahmad");
+		// TODO Auto-generated method stub
 
 	}
 
