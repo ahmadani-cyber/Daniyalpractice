da73fdcfdcabe103e9867544a695b7aa1b417d09
Daniyal <butt.daniyalahmad1992@gmail.com>
2020-04-23 13:28:45 -0400

    commitagain

---
+++ GitPractice/src/Gitname/Againpract.java
@@ -0,0 +1,15 @@
+package Gitname;
+
+public class Againpract {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		System.out.println("my practice");
+		// TODO Auto-generated method stub
+
+	}
+
+}
